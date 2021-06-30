58236e9a3e97706b848dfd7447d7e6bd77c3635c
scheidelg <scheidelg@gmail.com>
2021-06-30 19:14:08 -0400

    Update stage2.ps1

--- stage2.ps1
+++ stage2.ps1
@@ -1,17 +1,26 @@
 #=============================================================================
 # This script runs as the second stage of the Wiki update, called from
 # wiki_update.ps1.  This script allows us to update content outside of the
-# directories under Git version control (i.e., outside of those noted above,
-# for which we ran 'git pull').
+# directories that are under Git version control (i.e., outside of those for
+# which we can run 'git pull').
 #
 # Commands run by this script are intended to either be (a) always executed
-# every time this script is run, or (b) executed only once.  In the case of
-# commands that are executed only once, a unique tag (a string of text) is
-# used to identify a given set of commands.  Before a set of commands is
-# executed, the file /scripts/stage2.tags is checked and the commands are
+# every time this script is run, (b) executed only once, or (c) executed only
+# when specifically requested.
+#
+# In the case of commands that are executed only once, a unique tag (a string
+# of text) is used to identify a set of commands.  Before a set of commands is
+# is executed, the file /scripts/stage2.tags is checked and the commands are
 # executed only if the tag is not found.  At the end of the set of commands,
 # the stage2.tags file is updated with that set's associated tag.  This
 # prevents that set of commands from being executed multiple times.
+#
+# A unique tag is also used to identify a set of commands that are executed
+# only when specifically requested.  In this case, the tag must be specified
+# on the command line.  Command-line arguments to wiki_update.ps1 are passed
+# when calling this stage2.ps1 script
+executed the command-line
+# arguments are 
 #
 # Examples of 
 #
