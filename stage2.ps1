48e8b699f7d006c64abe5033856022f26373296e
scheidelg <scheidelg@gmail.com>
2021-06-30 23:40:23 -0400

    Update stage2.ps1

--- stage2.ps1
+++ stage2.ps1
@@ -4,12 +4,12 @@
 # directories that are under Git version control (i.e., outside of those for
 # which we can run 'git pull').
 #
-# Commands run by this script are intended to either be (a) always executed
-# every time this script is run, (b) executed only once, or (c) executed only
-# when specifically requested.
+# Commands run by this script can either be (a) executed every time this
+# script is run, (b) executed only once, or (c) executed only when
+# specifically requested.
 #
-# In the case of commands that are executed only once, a unique tag (a string
-# of text) is used to identify a set of commands.  Before a set of commands is
+# In the case of commands that are executed only once, a unique tag - a string
+# of text - is used to identify a set of commands.  Before a set of commands
 # is executed, the file /scripts/stage2.tags is checked and the commands are
 # executed only if the tag is not found.  At the end of the set of commands,
 # the stage2.tags file is updated with that set's associated tag.  This
@@ -17,19 +17,22 @@
 #
 # A unique tag is also used to identify a set of commands that are executed
 # only when specifically requested.  In this case, the tag must be specified
-# on the command line.  Command-line arguments to wiki_update.ps1 are passed
-# when calling this stage2.ps1 script
-executed the command-line
-# arguments are 
-#
-# Examples of 
+# on the command line to this script.  This script is normally called from
+# the wiki_update.ps1 script; wiki_update.ps1 passes any command-line
+# arguments to this script.
 #
 #-----------------------------------------------------------------------------
 # 2020.06.29   GJS   Original script
 #-----------------------------------------------------------------------------
 
-# Use this section to run commands that are executed every time the Wiki
-# update process is run.
+# Use this section to execute commands that are executed every time the Wiki
+# update process is run.  No special checks are performed before executing
+# the commands.
+
+# Invoke-Expression -Command /scripts/stage2.ps1
+
+
+# Use this section to execute commands that are executed 
 
 # do whatever you need to do...
 
